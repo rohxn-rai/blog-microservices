519083207fe0070eafb86286ed54a02a7b305d7f
rohxn-rai <rohan.rai932@outlook.com>
2025-05-19 08:16:31 -0400

    basic posts service

---
+++ posts/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { randomBytes } from "crypto";
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(400, () => {
+  console.log("Listening on 4000");
+});
