5369f96a5c3229965085153c33e5b50097629ae2
rohxn-rai <rohan.rai932@outlook.com>
2025-05-20 05:37:39 -0400

    added a cors middleware

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { randomBytes } from "crypto";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
