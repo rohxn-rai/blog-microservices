f78f31803f6fa780b0943b32fc6c4cc438a2b275
rohxn-rai <rohan.rai932@outlook.com>
2025-05-20 08:30:30 -0400

    added a request to the event-bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import { randomBytes } from "crypto";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -22,6 +23,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postID: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
