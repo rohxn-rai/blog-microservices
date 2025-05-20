fc1903b01e20dbc7f1c30647a9cdbfd4d710f1db
rohxn-rai <rohan.rai932@outlook.com>
2025-05-20 05:37:58 -0400

    added form submit handler

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import PostCreate from "./PostCreate";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="container d-flex flex-column p-5 gap-2">
       <h1>Create Post</h1>
       <PostCreate />
     </div>
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
 const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit} className="d-flex flex-column gap-2">
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="form-control"
+          />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary w-25">Submit</button>
       </form>
     </div>
   );
