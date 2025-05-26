378c092ac063512eee23d3aae331fabac13a27a8
rohxn-rai <rohan.rai932@outlook.com>
2025-05-26 07:15:32 -0400

    moderation req handling - made changes to handle request and response to and from moderation service

--- query/index.js
+++ query/index.js
@@ -26,12 +26,13 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({
       id,
       content,
+      status,
     });
   }
 
