f5f5cd5346ef48591f259f884eab1c65d972c5bc
rohxn-rai <rohan.rai932@outlook.com>
2025-05-26 08:34:21 -0400

    added CommentUpdated event

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,16 +10,16 @@ app.post("/events", (req, res) => {
   const event = req.body;
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
-    console.log(err.message);
+    console.log(err.message, " on posts");
   });
   axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
+    console.log(err.message, " on comments");
   });
   axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
+    console.log(err.message, " on query");
   });
   axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
+    console.log(err.message, " on moderation");
   });
 
   res.send({ status: "OK" });
--- query/index.js
+++ query/index.js
@@ -36,6 +36,20 @@ app.post("/events", (req, res) => {
     });
   }
 
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log(posts);
+
   res.send({});
 });
 
