ccbd2a8ce976b693e224ccebd32dd3e84cef3c50
rohxn-rai <rohan.rai932@outlook.com>
2025-05-27 04:46:04 -0400

    connected query to event-bus - connected event-bus to query for fetching previous data for when the query service is down

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,23 +6,31 @@ import axios from "axios";
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
   axios.post("http://localhost:4000/events", event).catch((err) => {
-    console.log(err.message, " on posts");
+    console.log(`${err.message} on posts.`);
   });
   axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message, " on comments");
+    console.log(`${err.message}, " on comments.`);
   });
   axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message, " on query");
+    console.log(`${err.message}, " on query.`);
   });
   axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message, " on moderation");
+    console.log(`${err.message}, " on moderation.`);
   });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
