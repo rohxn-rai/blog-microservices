2c438d54e8509266a7662c6943ba2ddc55c8a36d
rohxn-rai <rohan.rai932@outlook.com>
2025-05-28 08:30:53 -0400

    updated routes - updated the api routes for all services

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,13 +16,13 @@ app.post("/events", (req, res) => {
   axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
     console.log(`${err.message} on posts.`);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
     console.log(`${err.message} on comments.`);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
     console.log(`${err.message} on query.`);
   });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
     console.log(`${err.message} on moderation.`);
   });
 
